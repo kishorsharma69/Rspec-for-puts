6b07a7fe7632daddd791fa63af06fdeaa7c19bb5
Kishor Sharma <kishor_sharma@lftechnology.com>
2016-01-22 12:57:50 +0545

    test for fibonacci series

---
+++ class/fibonacci_series.rb
@@ -0,0 +1,40 @@
+def input_numb
+  puts "enter number"
+  numb = gets.chomp
+end
+# input_numb
+
+def fibonacci_series(n)
+  n1 = 0
+  n2 = 1
+  sum = 0
+  a = []
+  while n1 < n.to_i
+    #puts n1
+    a.push(n1)
+    sum = n1 + n2
+     n1 = n2
+    n2 = sum
+  end
+return a
+end
+
+# fibonacci_series(4)
+
+def print_fibonacci(a)
+=begin
+  a = input_numb
+  b = fibonacci_series(a)
+=end
+puts "the output is #{a}"
+  #puts "fibonacci  number #{b}"
+
+end
+
+
+def main
+  a = input_numb
+  b =  fibonacci_series(a)
+  print_fibonacci(b)
+end
+main
---
+++ class/fibonacci_spec.rb
@@ -0,0 +1,25 @@
+require './rspec_config.rb'
+require './fibonacci_series.rb'
+
+describe "fibonacci series" do
+
+  context "#input_numb" do
+    it"should show the fibonacci series upto the number user input" do
+      stub(:gets) {'20'}
+      expect{input_numb}. to output("enter number\n").to_stdout
+    end
+  end
+
+  context "#fibonacci_series" do
+    it "should give the fibonacci series" do
+      expect(fibonacci_series(2)). to eql([0,1,1])
+    end
+  end
+
+  context "#print_fibonacci" do
+    it "should print fibonacci series" do
+      expect{print_fibonacci(2)}.to output("the output is 2\n").to_stdout
+    end
+  end
+
+end
